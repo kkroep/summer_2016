2be96056783674b47b46d36d6ed0f2ecb0d54ea3
kkroep <k.kroep1@gmail.com>
2016-09-13 17:15:07 -0700

    a lot of high V range measurements

--- labVIEW/scratch/plot_reset.m
+++ labVIEW/scratch/plot_reset.m
@@ -21,8 +21,8 @@ end
 hold off;
 
 % legend(legend_names, 'location', 'northeastoutside');
-title('VBO reset voltage for pin 22-30');
+title('VBO reset voltage for pin 26');
 xlabel('input voltage [V]');
 ylabel('VBO during reset [V]');
 print('-dpdf', '-color', fullfile(pwd, 'plot_reset.pdf'));
-print('-deps', '-color', fullfile(pwd, '../../report/fig/pin22-30_reset_-25-0V.eps')) 
+print('-deps', '-color', fullfile(pwd, '../../report/fig/pin26_reset.eps')) 
--- labVIEW/scratch/plot_slope.m
+++ labVIEW/scratch/plot_slope.m
@@ -13,17 +13,19 @@ j=1;
 for i=1:size(tmp,1)
     if(strncmp(tmp(i).name, "GaN_", 4))
         C = csvread(tmp(i).name);
-        C(:,2) = -C(:,2)*400e-15-1.2e-10;
-        plot(C(:,1), C(:,2), 'linewidth', 2, 'Color', colorspec{mod(j,12)+1});
+        C(:,2) = abs(C(:,2).*10^-15);
+        semilogy(C(:,1), C(:,2), '.','linewidth', 2, 'Color', colorspec{mod(j,12)+1});
         legend_names(j,:)=['pin', tmp(i).name(5:6)];
         j=j+1;
     end
 end
 hold off;
 
-legend(legend_names, 'location', 'northeastoutside');
-title('I/V curve for pin 22-30');
+% legend(legend_names, 'location', 'northeastoutside');
+legend('out', 'out', 'out', 'vbo', 'vbo interval', 'location', 'northeastoutside');
+% axis([-100 0 0 1e-5]);
+title('I/V curve for pin 21');
 xlabel('input voltage [V]');
 ylabel('Current [A]');
 print('-dpdf', '-color', fullfile(pwd, 'plot_slope.pdf'));
-print('-deps', '-color', fullfile(pwd, '../../report/fig/pin22-30_slope_-25-0V.eps'))
+print('-deps', '-color', fullfile(pwd, '../../report/fig/pin26_slope.eps'))
